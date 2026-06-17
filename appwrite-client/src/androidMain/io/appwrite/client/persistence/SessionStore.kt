5a87e1aa93d1fa29dbe15c2bc1fd8f42c1c2c372
MacBook Jonte <walterjonte@icloud.com>
2026-06-17 14:25:32 +0200

    Add SessionStore file for Android Verions

---
+++ appwrite-client/src/androidMain/io/appwrite/client/persistence/SessionStore.kt
@@ -0,0 +1,28 @@
+package io.appwrite.client.persistence
+
+import android.content.Context
+import android.content.SharedPreferences
+
+actual class SessionStore actual constructor() {
+    
+    private val prefs: SharedPreferences = AndroidContextProvider.context.getSharedPreferences(
+        "io.appwrite.session",
+        Context.MODE_PRIVATE
+    )
+
+    actual fun save(key: String, value: String) {
+        prefs.edit().putString(key, value).apply()
+    }
+
+    actual fun load(key: String): String? {
+        return prefs.getString(key, null)
+    }
+
+    actual fun remove(key: String) {
+        prefs.edit().remove(key).apply()
+    }
+
+    actual fun clear() {
+        prefs.edit().clear().apply()
+    }
+}
